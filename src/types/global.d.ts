1f348d510081b63f4beeb1ff3bb3f98df4cf6fca
Shrysxs <shreyashborade2@gmail.com>
2025-09-15 22:29:30 +0530

    chore(types): remove custom JSX IntrinsicElements override to satisfy ESLint (no-explicit-any)

--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-// Ensure JSX namespace is available for TS when next-env.d.ts is not present in the editor context.
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: string]: any;
-  }
-}
